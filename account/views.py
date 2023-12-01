5346e5fbb10168f9d155f35ecc53a3f93bf9236e
itdevx <testwebsite1244@gmail.com>
2023-12-01 06:53:56 -0800

    change login, register and profile view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class LoginView(View):
+    template_name = 'login.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class RegisterView(View):
+    template_name = 'register.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+
+class ProfileView(View):
+    template_name = 'profile.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
