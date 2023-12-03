40e078cef4842501f50f3de1a08e8d5908b5e37e
itdevx <testwebsite1244@gmail.com>
2023-12-03 06:24:11 -0800

    adding all templates and change that and make models

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from account.views import *
+
+
+app_name = 'account'
+
+urlpatterns = [
+    path('login', LoginView.as_view(), name='login'),
+    path('register', RegisterView.as_view(), name='register'),
+    path('profile', ProfileView.as_view(), name='profile'),
+    path('forgot', ForgotView.as_view(), name='forgot'),
+]
